4d18e42472dceec08955788f733e609ec8d4724e
BekKing09 <satimovali91@gmail.com>
2025-12-26 09:06:42 +0500

    satr

---
+++ satr_javob.py
@@ -0,0 +1,88 @@
+
+# ism = input("ism kiriting:")
+# print("ismingiz:>>>", ism.upper())
+# print("ismingiz:>>>", ism.lower())
+# print("ismingiz:>>>", len(ism))
+
+
+
+
+# ism = input("ism kiriting:")
+# t_ism = ism[::-1]
+# print("ismingiz teskari:>>", t_ism)
+
+# text = "python"
+# print(text.find("p"))
+# print(text.find("x"))
+
+
+
+# matn = input("matn kiriting:>>>")
+# if matn.isalpha():
+#     print("bu matn faqat harfdan iborat")
+# elif matn.isdigit():
+#     print("bu matn faqat sondan iborat")
+# else:
+#     print("bu matnda son va harflar bor")
+
+
+# s = "Ananas va banan"
+# print(s.lower().count("a"))
+
+
+
+# s = "   Ananas va banan            "
+# print(s.strip())
+
+
+# matn = "men python dasturlash tilini organaman"
+# print(matn.title())
+
+
+# words = input("matn kiriting:>>>")
+# r_words = words[::-1]
+# if r_words == words:
+#     print("bu so'z palindrom")
+# else:
+#     print("bu so'z palindrom emas")
+
+
+# name_and_surname = input("ism va familyangizni kiriting:>>>")
+# print("salom", name_and_surname.title())
+
+
+# words = input("matn kiriting:>>>")
+# if words.lower().startswith("python"):
+#     print("bu matn 'python' bilan boshlanadi")
+# elif words.lower().endswith("uz"):
+#     print("bu matn 'uz' bilan tugaydi")
+# else:
+#     print("bu matn 'python' bilan boshlanmaydi 'uz' bilan tugamaydi")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+number = input("son kiriting:>>>")
+print(number.zfill(5))
+
+
+
+
+
+
